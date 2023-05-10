b7f1b9a2fa1ddd127943487d97f488729d906cfa
freginaldo <felipe.reginaldo@outlook.com>
2023-05-10 09:33:26 -0300

    Primeiro Commit

---
+++ PortalClientes/PortalClientes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PortalClientes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
